343ac6bb1634d0dc48b999fcaadb5297172522dd
JEFF Liao <jeffliao92316046@gmail.com>
2024-09-02 16:07:29 +0800

    fix :docker build waring

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -2,19 +2,20 @@
 
 CMD=${1}
 
-IMG_NAME=app-template
+IMG_NAME=reserve
 DOCKER_COMPOSE_NAMESPACE=$IMG_NAME
 
 if [ "$CMD" == "SERVICE_UP" -a "$#" == "3" ]; then
     BUILD_BACKEND_VERSION=${2}
     BUILD_FRONTEND_VERSION=${3}
-    docker-compose -f docker/docker-compose-db.yaml up -d
     echo "Run SERVICE_UP(BUILD_BACKEND_VERSION=$BUILD_BACKEND_VERSION, BUILD_FRONTEND_VERSION=$BUILD_FRONTEND_VERSION)"
     export BACKEND_VERSION=$BUILD_BACKEND_VERSION 
     export FRONTEND_VERSION=$BUILD_FRONTEND_VERSION
-    docker-compose -f docker/docker-compose-api.yaml up -d
-    docker-compose -f docker/docker-compose-ui.yaml up -d
-    docker-compose -f docker/docker-compose-nginx.yaml up -d
+    docker-compose  -p $DOCKER_COMPOSE_NAMESPACE \
+                    -f docker/docker-compose-db.yaml \
+                    -f docker/docker-compose-api.yaml \
+                    -f docker/docker-compose-ui.yaml \
+                    -f docker/docker-compose-nginx.yaml up -d 
 
 elif [ "$CMD" == "UPDATE_IMAGE" -a "$#" == "3" ]; then
     BUILD_BACKEND_VERSION=${2}
@@ -22,8 +23,9 @@ elif [ "$CMD" == "UPDATE_IMAGE" -a "$#" == "3" ]; then
     echo "Run UPDATE_IMAGE(BUILD_BACKEND_VERSION=$BUILD_BACKEND_VERSION, BUILD_FRONTEND_VERSION=$BUILD_FRONTEND_VERSION)"
     export BACKEND_VERSION=$BUILD_BACKEND_VERSION 
     export FRONTEND_VERSION=$BUILD_FRONTEND_VERSION
-    docker-compose -f docker/docker-compose-api.yaml up -d
-    docker-compose -f docker/docker-compose-ui.yaml up -d
+    docker-compose  -p $DOCKER_COMPOSE_NAMESPACE \
+                    -f docker/docker-compose-api.yaml\
+                    -f docker/docker-compose-ui.yaml up -d 
     echo "restart nginx"
     docker restart nginx
     echo "delete old images"
